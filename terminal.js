d0f04d0bcfeff020008e63f50baab170272c0842
Mac Lawson <macattackairman@gmail.com>
2022-06-28 14:40:42 -0400

    more changes to DMP Terminal

---
+++ terminal.js
@@ -0,0 +1,16 @@
+let file = "~/.bash_history"
+let reader = new FileReader();
+// let text = reader.readAsText(file);
+text = "This text is a replacement of what is normally the ~/.bash_history file, replace my VARIABLE!"
+const output = document.getElementById("outputField");
+output.textContent = text;
+
+function reloadPage() {
+    location.reload();
+}
+
+function changeFile() {
+    let new_file = document.getElementById('CHANGED_FILE').value;
+    let new_text = reader.readAsText(new_file)
+    output.textContent= new_text;
+}
