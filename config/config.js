9f86da5f75236abdf42015fad0c854f2f03f6990
Mac Lawson <macattackairman@gmail.com>
2022-06-29 10:16:48 -0400

    working on server config and pinger

---
+++ config/config.js
@@ -0,0 +1,14 @@
+
+
+function pingServer() {
+    let ip = document.getElementById("ip_grab").value;
+    console.log("Server IP logged as:", ip);
+   // const Http = new XMLHttpRequest();
+   // const url='https://jsonplaceholder.typicode.com/posts';
+   // Http.open("GET", url);
+   // Http.send();
+
+  //  Http.onreadystatechange = (e) => {
+   // console.log(Http.responseText)
+    }
+
