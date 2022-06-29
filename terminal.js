179f08bee1b190d7760bf32fc2d6f6199b75c152
Mac L <macattackairman@gmail.com>
2022-06-29 07:44:16 -0400

    all sorts of stuff

--- terminal.js
+++ terminal.js
@@ -1,9 +1,4 @@
-let file = "~/.bash_history"
-let reader = new FileReader();
-// let text = reader.readAsText(file);
-text = "This text is a replacement of what is normally the ~/.bash_history file, replace my VARIABLE!"
-const output = document.getElementById("outputField");
-output.textContent = text;
+
 
 function reloadPage() {
     location.reload();
@@ -11,11 +6,13 @@ function reloadPage() {
 
 function changeFile() {
     let new_file = document.getElementById('CHANGED_FILE').value;
-    console.log(new_file);
-    let new_text = reader.readAsText(new_file)
-    output.textContent= new_text;
+
 }
 
-function createCall() {
-    
+function createNewUser() {
+    document.cookie = "status = started";
+    document.cookie = "user = active";
+    document.getElementById("output_area").innerHTML = document.cookie;
+
 }
+
