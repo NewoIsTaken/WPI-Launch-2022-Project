e3ee2b323ca5bff66d96c813efad1d69883c5df7
Mac Lawson <macattackairman@gmail.com>
2022-06-29 10:34:58 -0400

    added an input field for the docker server ip

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@
 function pingServer() {
     let ip = document.getElementById("ip_grab").value;
     console.log("Server IP logged as:", ip);
+    document.getElementById("server_ip").innerText = ("Server IP set as: " + ip);
    // const Http = new XMLHttpRequest();
    // const url='https://jsonplaceholder.typicode.com/posts';
    // Http.open("GET", url);
